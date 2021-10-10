5dd4c390deae5eefb3f950e46e848db91acbb630
Hossam Morsey <hosammorsey@gmail.com>
2021-10-10 23:40:47 +0300

    [Update] Style and functionality

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,26 +1,36 @@
 // libraries imports
 import React from "react";
-import { Button, Row } from "antd";
+import { Row, Col, Divider } from "antd";
 // components imports
 import AddToList from "../AddToDo";
 import ShowList from "../ShowList";
 import DoneList from "../DoneList";
+import { mainIcon2 } from "../../utils/utlis";
 import "./Container.scss";
 
 const Container = () => {
   return (
-    <>
-      <Row className="header-text">
-        Hey There ! It's the container Component
+    <div>
+      <Row className="header-row">
+        <img src={mainIcon2} alt="Main Icon" className="main-icon" />
       </Row>
-      <Button> Hey there ! </Button>
-      <div className="first-part-container">
-        <AddToList />
-        <DoneList />
-      </div>
 
-      <ShowList />
-    </>
+      <Divider />
+
+      <Row
+        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+        className="first-row-section"
+      >
+        <Col className="add-to-do">
+          <AddToList />
+        </Col>
+        <Col className="show-list" style={{ backgroundColor: "brown" }}>
+          <ShowList />
+        </Col>
+      </Row>
+
+      <DoneList />
+    </div>
   );
 };
 
