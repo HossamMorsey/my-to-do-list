f747df71802602d10e3ae599b43455556b1fea8c
Hossam Morsey <hosammorsey@gmail.com>
2021-10-10 23:40:22 +0300

    [Update] Functionality and styles

--- src/components/AddToDo/AddToDo.js
+++ src/components/AddToDo/AddToDo.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input, Button } from "antd";
+import { ClearOutlined, PlusOutlined } from "@ant-design/icons";
+import "./AddToDo.scss";
 
 const AddToDo = () => {
-  return <div>Hey There ! It's the Add To Do Component</div>;
+  const [state, setState] = useState({
+    toDoTask: "",
+  });
+
+  const onChangeText = (e) => {
+    setState({
+      ...state,
+      toDoTask: e.target.value,
+    });
+  };
+
+  const onPressEnter = (e) => {
+    setState({
+      ...state,
+      toDoTask: e.target.value,
+    });
+  };
+
+  const clearTextArea = () => {
+    setState({
+      ...state,
+      toDoTask: "",
+    });
+  };
+
+  return (
+    <div className="add-todo-text">
+      <Input
+        onChange={onChangeText}
+        onPressEnter={onPressEnter}
+        placeholder="Add your Task"
+        value={state.toDoTask}
+      />
+      <Button onClick={clearTextArea} icon={<ClearOutlined />} />
+      <Button icon={<PlusOutlined />} />
+    </div>
+  );
 };
 
 export default AddToDo;
