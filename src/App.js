c0e525a6f3acaac36aae849ef1d8251db9e079ae
Hossam Morsey <hosammorsey@gmail.com>
2021-10-10 23:39:44 +0300

    [Update]Remove unused style and code in App.js/css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header>Here will be the header section, a pic or something with a padding and margin or whatever </header>
+      {/* <header>Here will be the header section, a pic or something with a padding and margin or whatever </header> */}
       <Container />
     </div>
   );
