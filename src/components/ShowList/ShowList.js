9d575317fc7f764c2c1fcf010e87873c6282b2ed
Hossam Morsey <hosammorsey@gmail.com>
2021-10-14 01:53:20 +0300

    [Update] Add table to show list

--- src/components/ShowList/ShowList.js
+++ src/components/ShowList/ShowList.js
@@ -1,19 +1,36 @@
 import React from "react";
+import { Button, Checkbox } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 import "./ShowList.scss";
 
 const ShowList = ({ tasksList }) => {
   const showingTasksList = tasksList.map((task, index) => {
     return (
-      <span className="list-item">
-        {" "}
-        {index + 1} {task}
-      </span>
+      <>
+        <tr>
+          <td className="numbering">{index + 1}</td>
+          <td className="single-task">{task}</td>
+          <td className="check-box">
+            <Checkbox className="checkbox" />
+          </td>
+          <td className="delete">
+            <Button icon={<DeleteOutlined />} />
+          </td>
+        </tr>
+      </>
     );
   });
   return (
-    <div>
-      <h3> To-Do List</h3>
-      <div className="list-container">{showingTasksList}</div>
+    <div className="show-list-container">
+      <table className="todo-tasks">
+        <tr>
+          <th>#</th>
+          <th>To-Do Tasks</th>
+          <th>Done !!</th>
+          <th> Delete</th>
+        </tr>
+        {showingTasksList}
+      </table>
     </div>
   );
 };
