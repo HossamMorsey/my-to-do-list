3d3f0d6a86b8a9fab36fc93651f30389655d1f68
Hossam Morsey <hosammorsey@gmail.com>
2021-10-19 17:07:51 +0300

    [Update] Functionality and Design for DoneList

--- src/components/DoneList/DoneList.js
+++ src/components/DoneList/DoneList.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./DoneList.scss";
 
-const DoneList = () => {
-  return <div>Hey There ! It's the DoneList Component</div>;
+const DoneList = ({ doneList }) => {
+  const showingDoneList = doneList.map((task, index) => {
+    return (
+      <>
+        <tr>
+          <td className="numbering">{index + 1}</td>
+          <td className="single-task">{task}</td>
+        </tr>
+      </>
+    );
+  });
+  return (
+    <div className="done-list-container">
+      <table className="done-tasks">
+        <tr>
+          <th>#</th>
+          <th>Done Tasks</th>
+        </tr>
+        {showingDoneList}
+      </table>
+    </div>
+  );
 };
 
 export default DoneList;
