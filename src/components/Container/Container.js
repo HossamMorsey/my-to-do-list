3c8978f80d07bf3e3fabb6d91b4f7c42c59d79a9
Hossam Morsey <hosammorsey@gmail.com>
2021-10-19 17:07:23 +0300

    [Update] Functionality and Design for Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -13,6 +13,8 @@ const Container = () => {
   // Show list component to show the list if all tasks
   const [state, setState] = useState({
     listOfTasks: [],
+    doneTasks: [],
+    listsSectionVisible: false,
   });
 
   // Set tasks that have been added in add toDo component
@@ -22,9 +24,22 @@ const Container = () => {
       listOfTasks: tasks,
     });
   };
+  const setTableVisible = (vis) => {
+    setState({
+      ...state,
+      listsSectionVisible: vis,
+    });
+  };
+  const setDoneTasks = (tasks) => {
+    setState({
+      ...state,
+      doneTasks: tasks,
+    });
+  };
+  // console.log("state", state.doneTasks)
 
   return (
-    <div>
+    <div style={{ width: "98%" }}>
       <Row className="header-row">
         <img src={mainIcon2} alt="Main Icon" className="main-icon" />
       </Row>
@@ -34,14 +49,31 @@ const Container = () => {
         className="first-row-section"
       >
         <Col className="add-to-do">
-          <AddToList tasks={state.listOfTasks} setTasks={setTasks} />
-        </Col>
-        <Col className="show-list">
-          <ShowList tasksList={state.listOfTasks} />
+          <AddToList
+            tasks={state.listOfTasks}
+            setTasks={setTasks}
+            setTableVisible={setTableVisible}
+          />
         </Col>
       </Row>
-
-      <DoneList />
+      {state.listsSectionVisible && (
+        <Row
+          gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+          className="second-row-section"
+        >
+          <Col className="show-list">
+            <ShowList
+              tasksList={state.listOfTasks}
+              doneTasks={state.doneTasks}
+              setDoneTasks={setDoneTasks}
+              setTableVisible={setTableVisible}
+            />
+          </Col>
+          <Col className="done-list">
+            <DoneList doneList={state.doneTasks} />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
